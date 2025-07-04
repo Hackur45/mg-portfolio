a2d5f84b17057f13ed7d9cd9c4c0903ae86b2bfb
Mandar <mandargurjar51@gmail.com>
2025-07-04 13:43:29 +0530

    new domain

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["holopin.me"], 
+    domains: ["holopin.me","assets.leetcode.com", "leetcode.com"], 
   },
 };
 
