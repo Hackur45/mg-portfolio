9153987d32afe872334e7cdbe4afd24e2ab294e2
Mandar <mandargurjar51@gmail.com>
2025-07-04 13:17:47 +0530

    next/Image config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["holopin.me"], 
+  },
 };
 
 export default nextConfig;
